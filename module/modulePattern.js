416ab7439362ff2ae491619b11d0c236498d9bd3
Charles Buczel <charles.buczel@gmail.com>
2017-09-27 21:30:17 +0100

    Module pattern

---
+++ module/modulePattern.js
@@ -0,0 +1,32 @@
+const customModule = (() => {
+  let privateVariable = 'foo';
+
+  let privateFunction = (value) => {
+    privateVariable = value;
+  };
+
+  return {
+    getPrivateVariable: () => privateVariable,
+    invokePrivateFunction: (value) => {
+      privateFunction(value);
+    }
+  }
+})();
+
+//tests
+
+if (customModule.hasOwnProperty('privateVariable')) {
+  throw Error('privateVariable should be private');
+}
+
+if (customModule.hasOwnProperty('privateFunction')) {
+  throw Error('privateFunction should be private');
+}
+
+if (!customModule.hasOwnProperty('getPrivateVariable')) {
+  throw Error('getPrivateVariable should be private');
+}
+
+if (!customModule.hasOwnProperty('invokePrivateFunction')) {
+  throw Error('invokePrivateFunction should be private');
+}
